6421a21f8f6d1af8599801d7b763ae2afec6e5e6
Kurilenko Sergei <kurilenko.zerg@gmail.com>
2022-11-12 00:42:38 +0300

    Edit

--- src/Stepik1_17_11.java
+++ src/Stepik1_17_11.java
@@ -21,8 +21,8 @@ public class Stepik1_17_11 {
         char[] ch = sc.nextLine().toCharArray();
         for (int i = 0; i < ch.length; i++) {
             if (ch[i] == '0' || ch[i] == '6' || ch[i] == '9') count++;
-            if (ch[i]=='8') count+=2;
-                   }
+            if (ch[i] == '8') count += 2;
+        }
         System.out.println(count);
     }
 }
